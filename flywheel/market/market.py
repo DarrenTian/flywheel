00140ea82dc5a27a4c5eabe69def3b9942d1a800
Jason <peilinzhou666@gmail.com>
2020-07-22 22:41:17 -0700

    implementation of crawl_stock_history_price

--- flywheel/market/market.py
+++ flywheel/market/market.py
@@ -10,34 +10,31 @@ def set_date(date):
     market_date = date
 
 def get_price_on_date(ticker, date):
-    # test data
-    if ticker == 'GOOG':
-        return 100
-    if ticker == 'PINS':
-        return 50
-    return 0
+    return get_price(ticker, date)
 
 def get_price(ticker):
     print("Get price for {} on {}".format(ticker, market_date))
+    market_date_format = str(market_date)[:10]
 
-    with open('./flywheel/market/stock_data.json', 'r') as json_file:
+    with open('stock_data.json', 'r') as json_file:
         stock_data = json.load(json_file)
         if ticker not in stock_data or market_date not in stock_data[ticker]:
-            return update_stock_data(stock_data, ticker, market_date)
+            return update_stock_data(stock_data, ticker, market_date_format)
         else:
-            return stock_data[ticker][market_date]
+            return stock_data[ticker][market_date_format]
 
-def update_stock_data(stock_data, ticker, market_date):
+def update_stock_data(stock_data, ticker, market_date_format):
     stock = yf.Ticker(ticker)
-    stock_history_price_dict = crawl_stock_history_price(stock, "100d", "60m")
+    stock_history_price_dict = crawl_stock_history_price(stock, "10d", "1d")
 
     # overwrites ticker history price data, updates the stock data and stores as json file
     stock_data[ticker] = stock_history_price_dict
     with open('stock_data.json', 'w') as json_file:
-        json.dump(stock_data, json_file)
-    return stock_data[ticker][market_date]
+        json.dump(stock_data, json_file, indent=4)
+    return stock_data[ticker][market_date_format]
 
-
-def crawl_stock_history_price(stock_ticker, period="100d", interval="60m"):
-    # TODO
-    return 100
+def crawl_stock_history_price(stock_ticker, period="10d", interval="1d"):
+    ticker_history_price = stock_ticker.history(period=period, interval=interval)
+    ticker_history_price.index = ticker_history_price.index.to_series().apply(lambda x: str(x)[:10])
+    print(ticker_history_price)
+    return ticker_history_price.to_dict('index')
