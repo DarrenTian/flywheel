a4c3e8aec24905b21ed782ef5d38c5a35dae52a5
Kevin Xu <kevin.xu.v@gmail.com>
2020-07-27 17:37:47 +0800

    Add sentry monitor

--- flywheel/consts.py
+++ flywheel/consts.py
@@ -4,3 +4,10 @@ class Market(object):
     UNKNOWN = 'UNKNOWN'
     NASDAQ = 'NASDAQ'
     NYSE = 'NYSE'
+
+
+class Environment(object):
+    DEV = 'development'
+    TESTING = 'testing'
+    STAGING = 'staging'
+    PROD = 'production'
--- flywheel/settings.py
+++ flywheel/settings.py
@@ -1,5 +1,7 @@
 from .utils.config import Config
+from .consts import Environment
 
+ENV = Config.get('ENV', Environment.DEV)
 STOCK_LIST = Config.get_list('STOCK_LIST', [
     'AAPL', 'AMZN', 'BABA', 'CRM', 'EBAY', 'FB', 'GOOG', 'INTC',
     'MSFT', 'NIKE', 'PINS', 'PYPL', 'RKUNY', 'SBUX', 'TSLA', 'TWTR', 'UBER'
@@ -7,3 +9,4 @@ STOCK_LIST = Config.get_list('STOCK_LIST', [
 TZ = Config.get('TZ', 'Asia/Shanghai')
 REDIS_URI = Config.get('REDIS_URI', 'redis://127.0.0.1:6379')
 DB_URL = Config.get('DB_URL', 'sqlite:///flywheel.db')
+SENTRY_DSN = Config.get('SENTRY_DSN')
