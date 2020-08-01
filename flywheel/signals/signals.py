c99a4bc257bc779281a6b60d70df2cd5330510f0
Willmay <silent.sea31@gmail.com>
2020-08-01 00:25:40 -0700

    Add ema diff func.

--- flywheel/signals/signals.py
+++ flywheel/signals/signals.py
@@ -5,8 +5,9 @@ import sys
 
 from datetime import date
 
-DUMMY_DATA_PATH_WINDOWS = 'C:\\Users\\silentsea\\Desktop\\projects\\flywheel\\flywheel\\market\\stock_data.json'
-DUMMY_DATA_PATH = '../market/stock_data.json'
+DUMMY_DATA_PATH_WINDOWS = "C:\\Users\\silentsea\\Desktop\\projects\\flywheel\\flywheel\\market\\stock_data.json"
+DUMMY_DATA_PATH = "../market/stock_data.json"
+MOD_CANDIDATES = ["Open", "High", "Low", "Close", "Volume", "Dividends", "Stock Splits"]
 
 def get_price():
     with open(DUMMY_DATA_PATH, 'r') as json_file:
@@ -16,17 +17,18 @@ def get_price():
 def process_market_data(market_data):
     flag = True
     for ticker in market_data:
-        ema = get_ema(market_data[ticker], 'Close')
+        ema = get_ema(market_data[ticker], "Close")
         print(ema)
         if flag:
             lineplot(ema.keys(), ema.values())
             flag = False
 
-# return zip(date, ema)        
+# return dict{zip(date, ema)}        
 def get_ema(stock_data, mod):
     ema = {}
     N = 0
     ema_t0 = 0
+    plt.figure()
     for date in stock_data:
         stock_data_daily = stock_data[date]
         N += 1
@@ -35,6 +37,44 @@ def get_ema(stock_data, mod):
         ema[date] = ema_t1
         ema_t0 = ema_t1
     return ema
+
+# return dict{zip(date, ema_dif)}
+def get_ema_dif(ema_dict, day_range_alpha, day_range_beta):
+    ema_tuples = ema_dict.items()
+    ema_dif = {}
+    if (len(ema) < day_range_beta):
+        return ema_dif
+    ema_slow = 0
+    ema_fast = 0
+    slow_head = 0
+    fast_head = 0
+    N_slow = 0
+    N_fast = 0
+    for i in range(len(ema_tuples)):
+        N_fast += 1
+        N_slow += 1
+        date, ema = ema_tuples[i]
+        ema_dif[date] = 0
+        if (N_fast >= day_range_alpha):
+            N_fast = day_range_alpha
+            ema_fast = culmulative_range_average(ema_fast, N_fast, ema, ema_tuples[fast_head][1])
+            fast_head += 1
+        else:
+            ema_fast = culmulative_range_average(ema_fast, N_fast, ema, 0)
+        if (N_slow >= day_range_beta):
+            N_slow = day_range_beta
+            ema_slow = culmulative_range_average(ema_slow, N_slow, ema, ema_tuples[slow_head][1])
+            slow_head += 1
+            ema_dif[date] = ema_fast - ema_slow
+        else:
+            ema_slow = culmulative_range_average(ema_slow, N_slow, ema, 0)
+    return ema_slow
+
+
+    
+
+def culmulative_range_average(pre_average, N, new_value, old_value):
+    return pre_average + (new_value - old_value) / N
 
 def lineplot(x_data, y_data, x_label="", y_label="", title=""):
     _, ax = plt.subplots()
@@ -45,9 +85,19 @@ def lineplot(x_data, y_data, x_label="", y_label="", title=""):
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
 
+def testplot():
+    plt.subplots(1, 1)
+    x= range(100)
+    y= [i**2 for i in x]
+
+    plt.plot(x, y, linewidth = '1', label = "test", color='#539caf', linestyle=':', marker='|')
+    plt.legend(loc='upper left')
+    plt.show()
+
 if __name__ == '__main__':
     #print(sys.path)
     #print(os.getcwd())
     #print(get_price("msft"))
     market_data = get_price()
     process_market_data(market_data)
+    #testplot()
